9fb844e2e653a4c4fc3d4dbf19bdb5188612b767
yash8245 <yashchaudhary9938@gmail.com>
2023-08-26 23:23:43 +0530

    First Commit

---
+++ src/features/user/userAPI.js
@@ -0,0 +1,28 @@
+export function fetchLoggedInUserOrders() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("/orders/own/");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchLoggedInUser() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("/users/own");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function updateUser(update) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("/users/" + update.id, {
+      method: "PATCH",
+      body: JSON.stringify(update),
+      headers: { "content-type": "application/json" },
+    });
+    const data = await response.json();
+    // TODO: on server it will only return some info of user (not password)
+    resolve({ data });
+  });
+}
